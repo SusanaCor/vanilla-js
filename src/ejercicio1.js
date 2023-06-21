fce67b17ff4dd9dcb098d67fdc28152c0fabcf85
SusanaCor <scorderoa@fwdcostarica.com>
2023-06-21 12:04:11 -0600

    ejrcicio1

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -2,7 +2,33 @@ let contador = {
   valor: 0,
   siguiente: function () {
     // Inserte el código aquí
+      this.valor = this.valor ++
+      console.log(this.valor)
+    return this.valor;
   },
 };
 
-export { contador }
+export { contador };
+
+// let saludo = function () {
+//   console.log("Hola Mundo");
+// };
+// let saludoTodos = function () {
+//   console.log("Hola Todos");
+// };
+
+// function mostrarMensaje(fn) {
+//   fn();
+// }
+
+// mostrarMensaje(saludo);
+
+// function sumar(...numeros) {
+//   return numeros.reduce((acumulador, valorActual) => {
+//     return acumulador + valorActual;
+//   }, 0);
+// }
+
+// const resultado = sumar(1, 2, 3, 4, 10);
+
+// console.log({ resultado });
