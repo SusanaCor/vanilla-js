20176487dfe565b1adbd0ec863a41472928cce4f
SusanaCor <scorderoa@fwdcostarica.com>
2023-06-22 15:49:20 -0600

    agregar ejercicio

---
+++ src/calculadora.js
@@ -0,0 +1,20 @@
+
+
+ function sumar(x, y) {
+   return x+y
+}
+ function resta (x, y) {
+    return x - y
+}
+ function multi(x, y) {
+    return x * y
+}
+ function divi(x, y) {
+    return x / y
+}
+
+let calculad={
+    sumar,resta,multi,divi
+}
+
+export {calculad}
